78ba8291b7db3c36eb0f687f4ed3aa0b6c1265c1
Jared Reich <jaredreich@gmail.com>
2016-08-22 12:36:39 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyComponent from './MyComponent'
+
+ReactDOM.render(<MyComponent name="Jane" />, document.getElementById('container'));
